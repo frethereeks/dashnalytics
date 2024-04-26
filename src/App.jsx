11943bb7a0c0667adb5a362a461e8ed286d36ba5
Frederick Jones <Frederickjones@gmail.com>
2024-04-26 17:04:27 +0100

    Create features/sidebar branch and commence the sidebar styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
 import React from 'react'
 import Header from './components/Header'
+import Sidebar from './components/Sidebar'
 
 
 export default function App() {
 
   return (
-    <div className='bg-primary'>
-      <Header />
-      <div className="flex flex-col gap-4 md:flex-row my-5 mx-4 py-5">
-        <div className="flex-1 shadow-default md:flex-3 bg-white shad rounded-md p-4 grid sm:grid-cols-2 md:grid-cols-3 gap-4"></div>
-        <div className="flex-1 shadow-default bg-white shad p-4"></div>
+    <main className='bg-main flex'>
+      <Sidebar />
+      <div className="flex-1 flex flex-col gap-4">
+        <Header />
+        <div className="flex flex-col gap-4 md:flex-row my-5 mx-4 py-5">
+          <div className="flex-1 shadow-default md:flex-3 bg-white shad rounded-md p-4 grid sm:grid-cols-2 md:grid-cols-3 gap-4"></div>
+          <div className="flex-1 shadow-default bg-white shad p-4"></div>
+        </div>
       </div>
-    </div>
+    </main>
   )
 }
---
+++ src/assets/images/index.js
@@ -0,0 +1,30 @@
+
+import wreen_action from "./wreen_action.jpg"
+import wreen_actionname from "./wreen_actionname.jpg"
+import wreen_busylady from "./wreen_busylady.jpg"
+import wreen_catvideo from "./wreen_catvideo.jpg"
+import wreen_coffee_leaf from "./wreen_coffee_leaf.jpg"
+import wreen_coffee from "./wreen_coffee.jpg"
+import wreen_graybg from "./wreen_graybg.jpg"
+import wreen_laptop from "./wreen_laptop.jpg"
+import wreen_outdoortype from "./wreen_outdoortype.jpg"
+// import wreen_videobg1 from "./wreen_videobg1.mp4"
+// import wreen_videobg2 from "./wreen_videobg2.mp4"
+// import wreen_videobg3 from "./wreen_videobg3.mp4"
+// import wreen_videohandwrite from "./wreen_videohandwrite.mp4"
+
+export {
+    wreen_action,
+    wreen_actionname,
+    wreen_busylady,
+    wreen_catvideo,
+    wreen_coffee_leaf,
+    wreen_coffee,
+    wreen_graybg,
+    wreen_laptop,
+    wreen_outdoortype,
+    // wreen_videobg1,
+    // wreen_videobg2,
+    // wreen_videobg3,
+    // wreen_videohandwrite,
+}
