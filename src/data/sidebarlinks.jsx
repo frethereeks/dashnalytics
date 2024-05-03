a5c8bc724bbf727d21109bd925f9290382bd3f2d
dave <obiorahdave@gmail.com>
2024-05-03 12:02:17 -0400

    add nav links in the sidebar from the sidebarlinks .jsx file created in the data folder

---
+++ src/data/sidebarlinks.jsx
@@ -0,0 +1,40 @@
+import {  FaBagShopping, FaDice, FaHouse, FaStore, FaTelegram, FaWallet } from "react-icons/fa6"
+
+export const sidebarlinks = [
+    {
+        id: "33341",
+        title: "Dashboard",
+        link: "/",
+        icon: <FaHouse />,
+    },
+    {
+        id: "33342",
+        title: "Wallet",
+        link: "/Wallet",
+        icon: <FaWallet />,
+    },
+    {
+        id: "33343",
+        title: "Prouducts",
+        link: "/Prouducts",
+        icon: <FaBagShopping />,
+    },
+    {
+        id: "33344",
+        title: "category",
+        link: "/category",
+        icon: <FaDice />
+    },
+    {
+        id: "33345",
+        title: "Transaction",
+        link: "/Transaction",
+        icon: <FaStore />
+    },
+    {
+        id: "33346",
+        title: "shipment",
+        link: "/shipment",
+        icon: <FaTelegram />
+    },
+]
